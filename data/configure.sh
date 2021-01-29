baf367adbbffc92bf45e9657657eeea71f79d817
Xakiru <xakiru@gmail.com>
2021-01-29 16:58:44 +0100

    Update configure.sh

--- data/configure.sh
+++ data/configure.sh
@@ -41,8 +41,7 @@ function install() {
 
     ls -la /tmp/${file_dir}
     cp -f /tmp/${file_dir}/frpc ${app_path}/
-    rm -rf /tmp/${file_name}
-    rm -rf /tmp/${file_dir}
+
     ls -la $app_path
 }
 
